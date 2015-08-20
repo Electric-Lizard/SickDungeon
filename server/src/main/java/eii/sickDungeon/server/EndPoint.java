12f49f4252052325fadb08d5168a1f156e956653
Electric-Lizard <Koldasov3@gmail.com>
2015-08-20 09:54:01 +0400

    Base maven configuration

---
+++ server/src/main/java/eii/sickDungeon/server/EndPoint.java
@@ -0,0 +1,11 @@
+package eii.sickDungeon.server;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by username on 8/19/15.
+ */
+@ServerEndpoint("lol")
+public class EndPoint {
+
+}
